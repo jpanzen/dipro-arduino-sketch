1bef6bf250048be9b2e6cfdfc9a35fa10c07afdf
Jan Panzenberger <jan@panzenberger.cz>
2024-07-08 22:05:44 +0200

    trafo - hotovo

--- trafo/trafo.ino
+++ trafo/trafo.ino
@@ -1,11 +1,11 @@
 #include <ZMPT101B.h>
-#include <Adafruit_INA219.h>
+#include <Adafruit_INA219.h> // K použití knihovny senzoru je třeba ji zahrnout
 
 // definování adresy senzoru
 #define ADDR 0x40
 // inicializace senzoru s nastavenou adresou z knihovny
 Adafruit_INA219 ina219_5(0x40);
-Adafruit_INA219 ina219_12(0x41);
+Adafruit_INA219 ina219_12(0x41); // I2C adresu senzoru s přepájenou ploškou jsme fyzicky změnili na 0x41, takže je potřeba přizpůsobit kód
 
 int state = 1;
 int lastState = -1; // Uložíme si poslední stav
@@ -15,17 +15,18 @@ void setup() {
   ina219_5.begin();
   ina219_12.begin();
 
+  // Příklad použití spínacího relé
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
 
-  digitalWrite(13, HIGH);
+  digitalWrite(13, HIGH); // HIGH - zapnuto, poloha switche je spojena s NO
   digitalWrite(12, HIGH);
-  digitalWrite(11, LOW);
+  digitalWrite(11, LOW); // LOW - vypnuto, poloha switche je spojena s NC
   digitalWrite(10, LOW);
 
-  ina219_5.setCalibration_16V_400mA();
+  ina219_5.setCalibration_16V_400mA(); // nastavili jsme nejnižší citlivost čidla
   ina219_12.setCalibration_16V_400mA();
 }
 
@@ -59,43 +60,36 @@ void loop() {
 }
 
 void set200z() {
-  Serial.println("Setting 200z"); // Debug výpis
   digitalWrite(13, HIGH);
   digitalWrite(12, HIGH);
 }
 
 void set400z() {
-  Serial.println("Setting 400z"); // Debug výpis
   digitalWrite(13, LOW);
   digitalWrite(12, LOW);
 }
 
 void set600z() {
-  Serial.println("Setting 600z"); // Debug výpis
   digitalWrite(13, LOW);
   digitalWrite(12, HIGH);
 }
 
 void set0r() {
-  Serial.println("Setting 200zr"); // Debug výpis
   digitalWrite(11, LOW);
   digitalWrite(10, LOW);
 }
 
 void set1r() {
-  Serial.println("Setting 200zr"); // Debug výpis
   digitalWrite(11, HIGH);
   digitalWrite(10, LOW);
 }
 
 void set2r() {
-  Serial.println("Setting 400zr"); // Debug výpis
   digitalWrite(11, LOW);
   digitalWrite(10, HIGH);
 }
 
 void set3r() {
-  Serial.println("Setting 600zr"); // Debug výpis
   digitalWrite(11, HIGH);
   digitalWrite(10, HIGH);
 }
@@ -105,13 +99,13 @@ void readVoltage() {
   unsigned long currentChrono = millis();
   if (currentChrono - lastChrono >= 1) { // ~100Hz
     Serial.print("Uin:");
-    Serial.print((230 * (analogRead(A0) - 512)) / 512);
+    Serial.print((230 * (analogRead(A0) - 512)) / 512); // Hodnoty z modulu lze získat pouze z formátu 0-5V, takže pro převod na voltu je potřeba kódově ošetřit
     Serial.print(",");
     Serial.print("Uout:");
     Serial.print((230 * (analogRead(A1) - 512)) / 512);
     Serial.print(",");
     Serial.print("proud1:");
-    Serial.print(ina219_5.getCurrent_mA());
+    Serial.print(ina219_5.getCurrent_mA()); // Čtení senzoru v mA
     Serial.print(",");
     Serial.print("proud2:");
     Serial.print(ina219_12.getCurrent_mA());
